8b613e3b9e9703a9d6df8d96ce71c68e17a07bed
Lucas levin <lucaslevin@gmail.com>
2021-12-19 10:53:23 +0800

    implement next-i18next, move translations

--- app/web/features/auth/username/Username.tsx
+++ app/web/features/auth/username/Username.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@material-ui/core";
 import { GetAccountInfoRes } from "proto/account_pb";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
 
 import { USERNAME_HELPER, YOUR_USERNAME_IS } from "../constants";
 
